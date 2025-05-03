9ecf8daf9e84fceb0bfaeef29a7029c05c0280e5
Eric Hooks <ebhooks21@gmail.com>
2025-05-03 00:06:43 -0500

    Added jquery to the project.

--- frontend/index.php
+++ frontend/index.php
@@ -13,6 +13,7 @@
 
 		<!-- JS Files -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>	
+		<script src="js/jquery-3.7.1.min.js"></script>	
 	</head>
 
 	<body>
