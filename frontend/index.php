5a1722270adbe64d34133a7a51cf9a94c4dbeef6
Eric Hooks <ebhooks21@gmail.com>
2025-05-03 00:01:17 -0500

    Added bootstrap javascript files from CDN.

--- frontend/index.php
+++ frontend/index.php
@@ -10,6 +10,9 @@
 
 		<!-- CSS Files -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">	
+
+		<!-- JS Files -->
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>	
 	</head>
 
 	<body>
