fcabf7061c9bf742b05bace042da851a3a7d8e29
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 22:14:50 -0500

    Start on projects listing

--- frontend/index.php
+++ frontend/index.php
@@ -10,6 +10,7 @@
 
 		<!-- CSS Files -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">	
+		<link href="css/style.css" rel="stylesheet">
 
 		<!-- JS Files -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>	
@@ -17,9 +18,13 @@
 	</head>
 
 	<body>
-		<div class="card col-11 bg-primary text-white projectlisting-area">
+		<div class="card col-11 bg-primary text-white projectlisting-area" id="projectlisting-area">
 				<div class="col-12">
 					<h2>Projects</h2>
+
+					<div class="card col-12 bg-secondary">
+
+					</div>
 				</div>
 		</div>
 	</body>
