7b20464cb1e969ab34fe17c632998a3ea7323a83
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 22:39:50 -0500

    Added javascript include for StoryKeeper.

--- frontend/index.php
+++ frontend/index.php
@@ -15,6 +15,7 @@
 		<!-- JS Files -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>	
 		<script src="js/jquery-3.7.1.min.js"></script>	
+		<script src="js/app/Storykeeper.js"></script>
 	</head>
 
 	<body>
---
+++ frontend/js/app/StoryKeeper.js
@@ -0,0 +1,4 @@
+/**
+ * Storykeeper.js -- File to hold the StoryKeeper class, the main class for the application.
+ * Author: Eric Hooks, 2025
+ */
