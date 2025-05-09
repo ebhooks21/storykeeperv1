bad638e2de4e1cc6536bbd80d94a7a12e10c2e46
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 22:48:01 -0500

    Created the StoryKeeper.js file. Created the StoryKeeper class. Created a constructor for the StoryKeeper class.

--- frontend/js/app/StoryKeeper.js
+++ frontend/js/app/StoryKeeper.js
@@ -2,3 +2,12 @@
  * Storykeeper.js -- File to hold the StoryKeeper class, the main class for the application.
  * Author: Eric Hooks, 2025
  */
+
+class StoryKeeper {
+	/**
+	 * Main constructor for the StoryKeeper class.
+	 */
+	constructor() {
+		alert("HERE");
+	}
+}
