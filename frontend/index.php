77f02875098bd3fb949ab8e5bce6eb2f36ed8784
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 22:58:47 -0500

    Added an include for the ProjectListingArea.js file in index.php

--- frontend/index.php
+++ frontend/index.php
@@ -15,6 +15,8 @@
 		<!-- JS Files -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>	
 		<script src="js/jquery-3.7.1.min.js"></script>	
+
+		<script src="js/app/ProjectListingArea.js"></script>
 		<script src="js/app/StoryKeeper.js"></script>
 	</head>
 
