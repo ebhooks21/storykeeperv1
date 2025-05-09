f2b02cb7cd1b371fc275c061d8e2a2b3c9b567a4
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 22:58:00 -0500

    Created ProjectListingArea.js to hold the ProjectListingArea class, which will have functions for the ProjectListingArea. Created the ProjectListingArea class.

---
+++ frontend/js/app/ProjectListingArea.js
@@ -0,0 +1,8 @@
+/**
+ * ProjectListingArea.js -- File to contain the ProjectListingArea class, which controls the project listing area.
+ * Author: Eric Hooks, 2025
+ */
+
+class ProjectListingArea {
+
+}
