ea9540008ddde72a56b88121a359347bf3b64bc1
Eric Hooks <ebhooks21@gmail.com>
2025-05-08 23:49:56 -0500

    Created a model for the ProjectListing.

---
+++ frontend/js/models/ProjectListing.js
@@ -0,0 +1,15 @@
+/**
+ * ProjectListing.js -- File to hold the ProjectListing class, which is a model for the ProjectListing.
+ * Author: Eric Hooks, 2025
+ */
+class ProjectListing {
+	/**
+	 * Main constructor for the ProjectListing.
+	 */
+	constructor() {
+		let self = this;
+
+		//Array for the project listing
+		self.projects = [];
+	}
+}
